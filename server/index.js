c110c89be6fe72f4166a87cb1b9b4619c62da834
ryanakiyama <ryan.akiyama15@gmail.com>
2017-10-24 19:19:59 -0700

    Delete comments

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const app = express();
-// app.use('/static', express.static(path.join(__dirname, 'public')));
-const bodyParser = require('body-parser');
 
 app.get('/', function (req, res) {
   res.send('GET request received');
