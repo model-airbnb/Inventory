70fcd13ce78773b7614e983cfc39a33f7494d43f
ryanakiyama <ryan.akiyama15@gmail.com>
2017-10-24 17:40:34 -0700

    Create request handler for fake incoming listings

--- server/index.js
+++ server/index.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app = express();
 // app.use('/static', express.static(path.join(__dirname, 'public')));
 const bodyParser = require('body-parser');
+const dataGenerator = require('./data-generator');
+const db = require('../database/index.js');
+app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.send('GET request received');
+app.get('/reservations', function (req, res) {
+  var listings = dataGenerator.listingsGenerator();
+  // db.insertListings(listings, function(err, data) {
+  //   if (err) {
+  //     res.sendStatus(500);
+  //   } else {
+  //     res.json(data);
+  //   }
+  // });
+  db.insertListings(listings);
+  res.send();
 });
 
 app.listen(8080, function () {
