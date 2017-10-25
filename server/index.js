2d90c242eb57eeca170777c89e798fccee29fa67
ryanakiyama <ryan.akiyama15@gmail.com>
2017-10-24 19:08:43 -0700

    Remove database helper functions not fully implemented yet

--- server/index.js
+++ server/index.js
@@ -1,21 +1,11 @@
 const express = require('express');
 const app = express();
-// app.use('/static', express.static(path.join(__dirname, 'public')));
 const bodyParser = require('body-parser');
 const dataGenerator = require('./data-generator');
 const db = require('../database/index.js');
 app.use(bodyParser.json());
 
 app.get('/reservations', function (req, res) {
-  var listings = dataGenerator.listingsGenerator();
-  // db.insertListings(listings, function(err, data) {
-  //   if (err) {
-  //     res.sendStatus(500);
-  //   } else {
-  //     res.json(data);
-  //   }
-  // });
-  db.insertListings(listings);
   res.send();
 });
 
