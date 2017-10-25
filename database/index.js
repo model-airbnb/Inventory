28b762b54c90d534b84b2775dbc2cd7a3cbb7513
ryanakiyama <ryan.akiyama15@gmail.com>
2017-10-24 19:09:37 -0700

    Remove database helper methods not fullly implemented

--- database/index.js
+++ database/index.js
@@ -6,14 +6,3 @@ var connection = mysql.createConnection({
   database : 'inventory_database'
 });
 
-// var pgp = require('pg-promise')(/*options*/)
-// var db = pgp('postgres://root:inventory@localhost:5432/postgres')
-
-
-
-const insertListings = function (listings, callback) {
-  console.log(listings);
- 
-};
-
-module.exports.insertListings = insertListings;
