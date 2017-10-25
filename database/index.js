69cc4d3df5a3e03f4f82b03055af4332a57c435e
ryanakiyama <ryan.akiyama15@gmail.com>
2017-10-24 17:38:58 -0700

    Create database helper method skeleton to insert listings

--- database/index.js
+++ database/index.js
@@ -5,3 +5,15 @@ var connection = mysql.createConnection({
   password : '',
   database : 'inventory_database'
 });
+
+// var pgp = require('pg-promise')(/*options*/)
+// var db = pgp('postgres://root:inventory@localhost:5432/postgres')
+
+
+
+const insertListings = function (listings, callback) {
+  console.log(listings);
+ 
+};
+
+module.exports.insertListings = insertListings;
