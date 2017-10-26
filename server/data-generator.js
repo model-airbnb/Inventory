b6b4e90613b6b93b2f3985a8f1033bbcc294005b
ryanakiyama <ryan.akiyama15@gmail.com>
2017-10-25 19:17:53 -0700

    Implement availabilityGenerator
    Generates example availble dates and prices for existing listings

--- server/data-generator.js
+++ server/data-generator.js
@@ -1,5 +1,5 @@
 const listingInfo = ["Beautiful", "Nice", "Cozy", "Spacious", "Luxurious", "Quiet", "Peaceful"];
-const hosts = ["Ryan Akiyama"];
+const hosts = ["Ryan Akiyama", "Tiffany Pham", "Tyler Truong", "Christine Wong"];
 const roomTypes = ["shared room", "private", "entire home"];
 const neighborhoods = [ 
                         "Financial District", 
@@ -15,37 +15,40 @@ const neighborhoods = [
 
 const listingsGenerator = function() {
   const listings = [];
-  for (var i = 0; i < 3; i++) {
+  for (var i = 0; i < 50000; i++) {
     let hostName = hosts[Math.floor(Math.random() * hosts.length)];
-    let superHost = Math.floor(Math.random() * 2);
     let neighborhood = neighborhoods[Math.floor(Math.random() * neighborhoods.length)];
     let roomType = roomTypes[Math.floor(Math.random() * roomTypes.length)];
-    let houseAmenities = [1, 3, 4, 5];
-    let listingName = `${listingInfo[Math.floor(Math.random() * listingInfo.length)]} + " " + ${roomType} + " house in " + ${neighborhood}`;
-    let hostRating = Math.floor(Math.random() * 101);
+    let listingName = `${listingInfo[Math.floor(Math.random() * listingInfo.length)]} ${roomType} in ${neighborhood}`;
+    let listingRating = Math.floor(Math.random() * 101);
     let numberOfRatings = Math.floor(Math.random() * 101);
-    let listingDate = new Date(2017, Math.floor(Math.random() * 3) + 9, Math.floor(Math.random() * 32) + 1);
+    let listingDate = `2017-${Math.floor(Math.random() * 3) + 10}-${Math.floor(Math.random() * 31) + 1}`;
     let listingPrice = Math.floor(Math.random() * 251) + 250;
     listings[i] = { 
                     listing_name: listingName,
                     host_name: hostName,
-                    super_host: superHost,
                     destinations_name: 'San Francisco',
                     neighborhoods_name: neighborhood,
                     room_type: roomType,
-                    amenities: houseAmenities,
-                    nightly_prices: [
-                                      {
-                                        date: listingDate,
-                                        price: listingPrice
-                                      },
-                                    ],
-                    is_available: 1,
-                    rating: hostRating,
+                    rating: listingRating,
                     number_of_ratings: numberOfRatings
                   };  
   }
   return listings;
 }
 
-module.exports.listingsGenerator = listingsGenerator;
+const availabilityGenerator = function() {
+  const availability = [];
+  for (var i = 0; i < 50000; i++) {
+    availability[i] = { 
+                        listings_id: Math.floor(Math.random() * 500000),
+                        date: `2017-${Math.floor(Math.random() * 3) + 10}-${Math.floor(Math.random() * 30) + 1}`,
+                        price: Math.floor(Math.random() * 251) + 250,
+                        is_available: 1
+                      };  
+  }
+  return availability;
+}
+
+module.exports.listingsGenerator = listingsGenerator
+module.exports.availabilityGenerator = availabilityGenerator;
