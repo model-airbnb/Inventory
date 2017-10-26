8f10278104319127f5470956989264a8879f6820
ryanakiyama <ryan.akiyama15@gmail.com>
2017-10-25 19:21:58 -0700

    Implement a request handler for incoming availability updates for listings

--- server/index.js
+++ server/index.js
@@ -5,7 +5,31 @@ const dataGenerator = require('./data-generator');
 const db = require('../database/index.js');
 app.use(bodyParser.json());
 
-app.get('/reservations', function (req, res) {
+app.post('/listings', function (req, res) {
+  var listings = dataGenerator.listingsGenerator();
+    for(var i = 0; i < listings.length; i++) {
+      db.insertListings(listings[i], i, function(err, data) {
+        if (err) {
+          console.log("Error", err);
+          res.sendStatus(500);
+        } 
+      });
+    }  
+  console.log("Finished");    
+  res.send();
+});
+
+app.post('/availability', function (req, res) {
+  var availableListings = dataGenerator.availabilityGenerator();
+    for(var i = 0; i < availableListings.length; i++) {
+      db.insertAvailability(availableListings[i], i, function(err, data) {
+        if (err) {
+          console.log("Error", err);
+          res.sendStatus(500);
+        } 
+      });
+    }  
+  console.log("Finished");    
   res.send();
 });
 
