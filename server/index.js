a1109cf1615f4886efa3315ee3bfa8c51fd7bd63
ryanakiyama <ryan.akiyama15@gmail.com>
2017-11-03 15:24:30 -0700

    Remove console.logs

--- server/index.js
+++ server/index.js
@@ -1,33 +1,36 @@
 const express = require('express');
-
-const app = express();
 const dataGenerator = require('./data-generator');
 const db = require('../database/index.js');
 const fs = require('fs');
 const AWS = require('aws-sdk');
+
 const sqs = new AWS.SQS({ region: 'us-west-1' });
+const app = express();
+
 
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
-
-const generate = () => {
+//writes new listings to a file to import
+const writeListings = () => {
   setInterval(() => {
     const listings = dataGenerator.listingsGeneratorImport();
     for (let i = 0; i < listings.length; i += 1) {
       const content = listings[i];
       fs.appendFile('./listings.csv', content + '\n', 'utf8', (err) => {
         if (err) {
+          console.log('Error', err);
           throw err;
         }
-        console.log('The file was saved!');
       });
     }
-    console.log('Finished');
   }, 5000);
 };
+
+//uncomment below to write listings to a file
+// writeListings();
 
 app.post('/listings', (req, res) => {
   const listings = dataGenerator.listingsGenerator();
@@ -37,23 +40,18 @@ app.post('/listings', (req, res) => {
         console.log('Error', err);
         res.sendStatus(500);
       }
-      console.log(data);
-
       //simulates a host listing
       const sqsParamsSearch = {
         MessageBody: JSON.stringify({ topic: 'Inventory', payload: data }),
         QueueUrl: process.env.SEARCH_QUEUE_URL,
       };
-
       sqs.sendMessage(sqsParamsSearch, (err, data) => {
         if (err) {
-          console.log('ERR', err);
+          console.log('Error', err);
         }
-        console.log(data);
       });
     });
   }
-  console.log('Finished');
   res.send();
 });
 
@@ -65,38 +63,31 @@ app.post('/availability', (req, res) => {
         console.log('Error', err);
         res.sendStatus(500);
       }
-      console.log(data);
-      //simulates a host listing
+      //simulates a new listing availability date
       const sqsParamsSearch = {
         MessageBody: JSON.stringify({ topic: 'Availability', payload: data }),
         QueueUrl: process.env.SEARCH_QUEUE_URL,
       };
-
       sqs.sendMessage(sqsParamsSearch, (err, data) => {
         if (err) {
-          console.log('ERR', err);
+          console.log('Error', err);
         }
-        console.log(data);
       });
     });
   }
-  console.log('Finished');
   res.send();
 });
 
 app.post('/update', (req, res) => {
   const updatedListings = dataGenerator.availabilityUpdateGenerator();
-  console.log('Start', Date.now());
   for (let i = 0; i < updatedListings.length; i += 1) {
     db.updateAvailability(updatedListings[i], (err, data) => {
       if (err) {
         console.log('Error', err);
         res.sendStatus(500);
       }
-      console.log(data);
     });
   }
-  console.log('Finished');
   res.send();
 });
 
