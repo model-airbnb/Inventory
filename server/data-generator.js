937d1e4788d5d8b42163aeb704d3cf854b7a131b
ryanakiyama <ryan.akiyama15@gmail.com>
2017-11-03 09:46:23 -0700

    Refactor data generator to multiple listing dates in one object

--- server/data-generator.js
+++ server/data-generator.js
@@ -1,6 +1,7 @@
-const listingInfo = ['Beautiful', 'Nice', 'Cozy', 'Spacious', 'Luxurious', 'Quiet', 'Peaceful'];
+const listingInfo = ['Beautiful', 'Nice', 'Cozy', 'Spacious', 'Luxurious', 'Quiet', 'Peaceful', 'Big', 'Small', 'Large', 'Stunning', 'Comfy'];
 const hosts = ['Ryan Akiyama', 'Tiffany Pham', 'Tyler Truong', 'Christine Wong'];
-const roomTypes = ['shared room', 'private', 'entire home'];
+const roomTypes = ['private room', 'entire home/apt', 'shared room'];
+const destinations = ['San Francisco'];
 const neighborhoods = [
   'Financial District',
   'Glen Park',
@@ -11,12 +12,18 @@ const neighborhoods = [
   'Outer Richmond',
   'Outer Sunset',
   'Presidio',
+  'Presidio Heights',
+  'Russian Hill',
+  'Seacliff',
+  'South of Market',
+  'Twin Peaks',
 ];
 
 const listingsGenerator = () => {
   const listings = [];
-  for (let i = 0; i < 50000; i++) {
+  for (let i = 0; i < 5000; i += 1) {
     const hostName = hosts[Math.floor(Math.random() * hosts.length)];
+    const destination = destinations[Math.floor(Math.random() * destinations.length)];
     const neighborhood = neighborhoods[Math.floor(Math.random() * neighborhoods.length)];
     const roomType = roomTypes[Math.floor(Math.random() * roomTypes.length)];
     const listingName = `${listingInfo[Math.floor(Math.random() * listingInfo.length)]} ${roomType} in ${neighborhood}`;
@@ -25,7 +32,7 @@ const listingsGenerator = () => {
     listings[i] = {
       listing_name: listingName,
       host_name: hostName,
-      destinations_name: 'San Francisco',
+      destinations_name: destination,
       neighborhoods_name: neighborhood,
       room_type: roomType,
       rating: listingRating,
@@ -35,16 +42,53 @@ const listingsGenerator = () => {
   return listings;
 };
 
+const listingsGeneratorImport = () => {
+  const listings = [];
+  for (let i = 0; i < 8000; i += 1) {
+    const neighborhood = neighborhoods[Math.floor(Math.random() * neighborhoods.length)];
+    const roomType = roomTypes[Math.floor(Math.random() * roomTypes.length)];
+    const listingName = `${listingInfo[Math.floor(Math.random() * listingInfo.length)]} ${roomType} in ${neighborhood}`;
+    const listingRating = Math.floor(Math.random() * 101);
+    const numberOfRatings = Math.floor(Math.random() * 101);
+    const hostsId = Math.ceil(Math.random() * 4);
+    const destinationsId = 1;
+    const neighborhoodsId = Math.ceil(Math.random() * 9);
+
+    //uncomment below to write listings to a .json file
+    // listings[i] = {
+    //   listing_name: listingName,
+    //   rating: listingRating,
+    //   rating_count: numberOfRatings,
+    //   room_type: roomType,
+    //   hosts_id: Math.ceil(Math.random() * 4),
+    //   destinations_id: 1,
+    //   neighborhoods_id: Math.ceil(Math.random() * 9),
+    // };
+
+    //uncomment below ot write listings to a .csv file
+    listings[i] = `"${listingName}",${listingRating},${numberOfRatings},${roomType},${hostsId},${destinationsId},${neighborhoodsId}`;
+  }
+  return listings;
+};
+
 //availble dates for existings listings are added
 const availabilityGenerator = () => {
   const availability = [];
-  for (let i = 0; i < 50000; i++) {
+  for (let i = 0; i < 50; i += 1) {
     availability[i] = {
-      listings_id: Math.floor(Math.random() * 500000),
-      date: `2017-${Math.floor(Math.random() * 3) + 10}-${Math.floor(Math.random() * 30) + 1}`,
-      price: Math.floor(Math.random() * 251) + 250,
+      listings_id: Math.floor(Math.random() * 10000000),
+      date: [
+        `2017-${Math.floor(Math.random() * 3) + 10}-${Math.floor(Math.random() * 30) + 1}`,
+        `2017-${Math.floor(Math.random() * 3) + 10}-${Math.floor(Math.random() * 30) + 1}`,
+        `2017-${Math.floor(Math.random() * 3) + 10}-${Math.floor(Math.random() * 30) + 1}`,
+      ],
+      price: [
+        Math.floor(Math.random() * 100) + 300,
+        Math.floor(Math.random() * 100) + 300,
+        Math.floor(Math.random() * 100) + 300,
+      ],
       is_available: 1,
-    }; 
+    };
   }
   return availability;
 };
@@ -52,11 +96,16 @@ const availabilityGenerator = () => {
 //availble dates for existings listings are updated
 const availabilityUpdateGenerator = () => {
   const availability = [];
-  for (let i = 0; i < 50000; i++) {
+  for (let i = 0; i < 5; i += 1) {
     availability[i] = {
-      listings_id: Math.floor(Math.random() * 500000),
-      date: `2017-${Math.floor(Math.random() * 3) + 10}-${Math.floor(Math.random() * 30) + 1}`,
-      price: Math.floor(Math.random() * 251) + 250,
+      listings_id: Math.floor(Math.random() * 10000000),
+      user_id: Math.floor(Math.random() * 10000000),
+      search_id: Math.floor(Math.random() * 10000000),
+      date: [
+        `2017-${Math.floor(Math.random() * 3) + 10}-${Math.floor(Math.random() * 30) + 1}`,
+        `2017-${Math.floor(Math.random() * 3) + 10}-${Math.floor(Math.random() * 30) + 1}`,
+        `2017-${Math.floor(Math.random() * 3) + 10}-${Math.floor(Math.random() * 30) + 1}`,
+      ],
       is_available: 0,
     };
   }
@@ -64,5 +113,6 @@ const availabilityUpdateGenerator = () => {
 };
 
 module.exports.listingsGenerator = listingsGenerator;
+module.exports.listingsGeneratorImport = listingsGeneratorImport;
 module.exports.availabilityGenerator = availabilityGenerator;
 module.exports.availabilityUpdateGenerator = availabilityUpdateGenerator;
